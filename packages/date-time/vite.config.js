46feff29bf569666d1c5b913ffe20c7284174b9d
gitgitWi <wiii@kakao.com>
2022-05-03 14:52:35 +0000

    Feat (prettier) add configs

--- packages/date-time/vite.config.js
+++ packages/date-time/vite.config.js
@@ -1,12 +1,12 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
     lib: {
-      entry: resolve(__dirname, "src/main.ts"),
-      name: "@getit-utils/date-time",
-      formats: ["es", "cjs"],
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: '@getit-utils/date-time',
+      formats: ['es', 'cjs'],
       fileName: (format) => `date-time.${format}.js`,
     },
   },
